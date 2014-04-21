0e6eb225c6793a84dae4851d528f3ed8fa26cce1
rcr-81 <raulcortesr@gmail.com>
2014-04-21 12:05:13 +0200

    Script start alfresco

---
+++ start_alfresco.sh
@@ -0,0 +1,5 @@
+#export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
+export JAVA_HOME=/opt/alfresco-3.4.13/java
+CATALINA_HOME="/home/smiledev/workspaces/workspace_UdL_3.4.13/Universitat_Lleida/target/alfresco/tomcat"
+/home/smiledev/workspaces/workspace_UdL_3.4.13/Universitat_Lleida/target/alfresco/tomcat/bin/catalina.sh jpda start
+tail -f /home/smiledev/workspaces/workspace_UdL_3.4.13/Universitat_Lleida/target/alfresco/tomcat/logs/catalina.out
