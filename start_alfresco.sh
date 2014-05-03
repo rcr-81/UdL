c22a06e2a576dda7562ca9002be8ee6dac7cdc52
rcr-81 <raulcortesr@gmail.com>
2014-05-03 18:48:15 +0200

    Cambio rutas JRE

--- start_alfresco.sh
+++ start_alfresco.sh
@@ -1,5 +1,4 @@
-#export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
-export JAVA_HOME=/opt/alfresco-3.4.13/java
+export JAVA_HOME=/usr/lib/jvm/jdk1.6.0_45/bin
 CATALINA_HOME="/home/smiledev/workspaces/workspace_UdL_3.4.13/Universitat_Lleida/target/alfresco/tomcat"
 /home/smiledev/workspaces/workspace_UdL_3.4.13/Universitat_Lleida/target/alfresco/tomcat/bin/catalina.sh jpda start
 tail -f /home/smiledev/workspaces/workspace_UdL_3.4.13/Universitat_Lleida/target/alfresco/tomcat/logs/catalina.out
--- stop_alfresco.sh
+++ stop_alfresco.sh
@@ -1,4 +1,3 @@
-#export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64
-export JAVA_HOME=/opt/alfresco-3.4.13/java
+export JAVA_HOME=/usr/lib/jvm/jdk1.6.0_45/bin
 CATALINA_HOME="/home/smiledev/workspaces/workspace_UdL_3.4.13/Universitat_Lleida/target/alfresco/tomcat"
 /home/smiledev/workspaces/workspace_UdL_3.4.13/Universitat_Lleida/target/alfresco/tomcat/bin/shutdown.sh
