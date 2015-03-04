4cfb2104287ea450916a4fb8a3985736855d29af
rcr-81 <raulcortesr@gmail.com>
2015-03-04 20:41:42 +0100

    Cambios varios

--- Universitat_Lleida_Alfresco_AMP/config/alfresco/templates/webscripts/udl/qdc/doRecalcularDates/doRecalcularDates.get.js
+++ Universitat_Lleida_Alfresco_AMP/config/alfresco/templates/webscripts/udl/qdc/doRecalcularDates/doRecalcularDates.get.js
@@ -1,10 +1,7 @@
 function main()
 {
 	impersonate.impersonate('admin');
-	var expedientsAgregacions = search.luceneSearch('ASPECT:"{http://www.smile.com/model/udlrm/1.0}expedient" OR ASPECT:"{http://www.smile.com/model/udlrm/1.0}agregacio"');
-	for (i = 0; i < expedientsAgregacions.length; i++){
-		dateHelper.runRecalcularDates(expedientsAgregacions[i].nodeRef);
-	}	
+	dateHelper.runRecalcularTotesDates();
 }
 
 main();
